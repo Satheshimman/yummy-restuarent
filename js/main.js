7a7ef07070f7e8d2e06497153e7f686867eea4a1
satheshimman <satheshimman17@gmail.com>
2023-10-25 11:39:32 +0530

    add

--- js/main.js
+++ js/main.js
@@ -13,26 +13,29 @@
 		 
 	 })
 	 
-	 
+ function sathesh(){
+    menulist.classList.remove("showmenu")
+    console.log("sathesh")
+ }    
 
 
 	 
 	 
-let owl = $(".carousel");
-owl.owlCarousel({
-    items:5,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+// let owl = $(".carousel");
+// owl.owlCarousel({
+//     items:5,
+//     loop:true,
+//     margin:10,
+//     autoplay:true,
+//     autoplayTimeout:1000,
+//     autoplayHoverPause:true
+// });
+// $('.play').on('click',function(){
+//     owl.trigger('play.owl.autoplay',[1000])
+// })
+// $('.stop').on('click',function(){
+//     owl.trigger('stop.owl.autoplay')
+// })
 
 	 
 
